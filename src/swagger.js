bdd2590979c0532f1b2abe73ed421359c5006fae
Baskar-L <lbaskar220@gmail.com>
2026-09-10 17:37:06 +0530

    update

--- src/swagger.js
+++ src/swagger.js
@@ -14,7 +14,7 @@ const options = {
 
     servers: [
       {
-        url: "http://localhost:5000",
+           url: process.env.API_BASE_URL || "http://localhost:5000",
         description: "Local development server"
       }
     ],
